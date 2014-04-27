627568b941f9b54bb2794bfc1b600a70406af586
Dima Maleev <diwingless@gmail.com>
2014-04-27 23:49:56 +0300

    adding ejs + some passport functions. i need to find more time:)

--- server/config/passport.js
+++ server/config/passport.js
@@ -2,6 +2,49 @@ var localStrategy = require('passport-local').Strategy
     ,user = require('./../models/model_user').User;
 
 
-//passport configuration goes here!
+module.exports = function(passport){
+
+  //========PASSPORT CONFIGURATION========//
+  
+  /**
+   * helper function to serialize user. quite simple now
+   */
+  passport.serializer(function(user, callback){
+    callback(null, user.id);
+  });
+
+  /**
+   * get user by id
+   */
+  passport.deserializeUser(function(id,callback){
+    user.findById(id, function(err, user){
+      callback(err, user);
+    });
+  });
+
+
+  passport.use('login', new localStrategy({
+    usernameField:'email',
+    passwordField:'password',
+    passReqToCallback:true
+  },
+
+  function(req, email, password, callback){
+    user.findOne({'local.email' : email}, function(err,user){
+      if (err)
+        return callback(err);
+      //validate if user exists
+      if (!user)
+        return callback(null, false, req.flash('loginMessage', 'No user found'));
+      
+      //check password
+      if (!user.validPassword(password))
+        return callback(null, false, req.flash('logineMessage', 'Incorrect password'));
+    
+      //success!
+      return callback(null, user);
+    });
+  }));
+};
 
 
