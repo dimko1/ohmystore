24c1cfb61cfad7358f8ae9cac6cde9d138772e7d
Dima Maleev <diwingless@gmail.com>
2014-04-28 21:27:49 +0300

    adding security methods

--- server/config/express_bootstrap.js
+++ server/config/express_bootstrap.js
@@ -1,9 +1,11 @@
-var express		= require('express')
-   ,http 		= require('http')
-   ,https		= require('https')
-   ,fs 			= require('fs')
-   ,path		= require('path')
-   ,util 		= require('util');
+var express		= require('express');
+var http 		= require('http');
+var https		= require('https');
+var fs 			= require('fs');
+var path		= require('path');
+var util 		= require('util');
+var passport    = require('passport');
+var flash       = require('connect-flash');
 
 /**
  * used to start express application 
@@ -15,16 +17,22 @@ function start(config){
 	// all environments
 	app.set('port', process.env.PORT || 8080);
 	app.set('views', path.join(__dirname, 'views'));
-	app.set('view engine', 'jade');
-	//app.use(express.favicon());
-	//app.use(express.logger('dev'));
+	app.set('view engine', 'ejs');
 	app.use(express.json());
 	app.use(express.urlencoded());
 	app.use(express.methodOverride());
-	//app.use(express.cookieParser('your secret here'));
-	app.use(express.session());
+	app.use(express.session({secret: 'thesecretphrase_wiskey'}));
+	
+	app.use(passport.initialize());
+	app.use(passport.session);
+
+	app.use(flash());
+
 	app.use(app.router);
 	app.use(express.static(path.join(__dirname, 'public')));
+
+	
+	require('./routes/routes.js')(app, passport);
 
 	//SSL required to deploy application for iOS starting 7.1
 	//can be self signed during development
@@ -39,6 +47,8 @@ function start(config){
 	https.createServer(options, app).listen(app.get('port'), function(){
 		console.log('Express server listening on port ' + app.get('port'));
 	}); */
+	
+
 
 	http.createServer(app).listen(config.port);
 
---
+++ server/routes/routes.js
@@ -0,0 +1,50 @@
+/**
+ * Routing information
+ */
+module.exports = function(app, passport){
+  
+  //==========================================
+  //HOME PAGE
+  //==========================================
+  //Shows index page
+  app.get('/', function(req, res){
+    res.render('index.ejs');
+  });
+  
+  //==========================================
+  //LOGIN PAGE
+  //==========================================
+  //Shows login page
+  app.get('/login', function(req, res){
+  	res.render('login.ejs');
+  });
+
+  //==========================================
+  //LOGOUT 
+  //==========================================
+  //Processes logout message
+  app.get('/logout', function(req,res){
+  	res.logout();
+  	res.redirect('/');
+  });
+  
+  //==========================================
+  //LOGOUT 
+  //==========================================
+  //Processes logout message
+  app.get('/profile', isLoggedIn, function(req,res){
+  	res.logout();
+  	res.redirect('profile.ejs');
+  });
+};
+
+/**
+ * Helper function to detect if user is authenticated
+ */
+function isLoggedIn(req, res, next){
+
+  if (req.isAuthenticated())
+    return next();
+  
+  res.redirect('/');
+}
